fb69a69fe2ed047bc84568f5ed01e5063d63d507
jinwook song <wlsdnr129@naver.com>
2024-12-11 15:16:17 +0900

    feat: packages, intl setup

---
+++ lib/view/home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:shorebird_demo/generated/l10n.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(S.of(context).title),
+      ),
+    );
+  }
+}
